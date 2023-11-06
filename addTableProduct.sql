dcc59b9350d1491963b59394215e5bcd9d597b74
AbdelazizBs <bensalemabdelaziz97@gmail.com>
2023-11-06 16:20:02 +0100

    etic europe dev web first push

---
+++ addTableProduct.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE Products (
+    Id               INT           NOT NULL    PRIMARY KEY IDENTITY,
+    Name              NVARCHAR (50) NOT NULL,
+    SubName          NVARCHAR (50) ,
+    Description      NVARCHAR (50) NOT NULL,
+    UserGuideFileName NVARCHAR (50) NOT NULL,
+    TechnicalSheet    NVARCHAR (50) NOT NULL,
+   RepaireCatalogue  NVARCHAR (50) NOT NULL
+);
